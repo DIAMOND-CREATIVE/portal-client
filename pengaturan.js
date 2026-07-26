6b0ad9627e854595a95034c48a009f124f3e274c
DIAMOND-CREATIVE <titiknol.studio14@gmail.com>
2026-07-26 18:44:33 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "DIAMOND CREATIVE",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
+    googleApiKey: "AIzaSyDkN22uxQwlTqhKe1rzY6JpBWX8hVoShEs",
     whatsappAdmin: "6285320021000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
